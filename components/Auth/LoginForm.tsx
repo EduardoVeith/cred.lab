9079092c2ea2757e85c0cbf4cfb5051e81767a9d
eduardo Collar Veith <edu.veith@gmail.com>
2025-03-18 20:34:25 -0300

    reestruturando pastas para o frontend

---
+++ components/Auth/LoginForm.tsx
@@ -0,0 +1,7 @@
+export default function LoginForm(){
+    return (
+        <form>
+        <h1>Login Form</h1>
+        </form>
+    );
+}
---
+++ components/Auth/RegisterForm.tsx
@@ -0,0 +1,7 @@
+export default function RegisterForm() {
+  return (
+    <form>
+      <h1>Register Form</h1>
+    </form>
+  );
+}
